77ce91018e7bd5efa1336d2f6a2c22ec2a0d9589
Hayden Linville <hayden.linville@gmail.com>
2022-04-04 16:18:12 -0400

    added post class in data, postCreate, PostDetail, and PostListItem on models. Create Postservice with methods. Made PostControll no methods added yet.

---
+++ 24Hours/24Hour.Data/Post.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24Hour.Data
+{
+    public class Post
+    {
+        [Key]
+        public int NoteId { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        public virtual List<CommentData> Comments { get; set; } = new List<CommentData>();
+
+    }
+}
